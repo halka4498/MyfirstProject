a816cddfb876fa704edde0dcd6a41cc4d96faf16
samim <samim@Samim>
2023-08-17 13:02:24 +0530

    changes to github

--- src/TestCases/amzn.java
+++ src/TestCases/amzn.java
@@ -12,8 +12,7 @@ public class amzn {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
-		Thread.sleep(4000);
+		
 		
 		JavascriptExecutor jse=(JavascriptExecutor)driver;
 		jse.executeScript("scrollBy (0,2000)");
